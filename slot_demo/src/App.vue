ef9fb9f8ff1fc6acc4f5c8df1a883a7be4dab9c8
Nalin Vaidya <nalinvaidya@gmail.com>
2020-05-04 11:28:32 +0700

    Added a application which demonstrate the Vue.js slot concept.

---
+++ slot_demo/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld>
+		<h1>Slot Example</h1>
+		<p>This is a basic slot</p>
+	</HelloWorld>
+	<NamedSlot>
+		<div slot="NamedSlot">
+			<h1>Name Slot Example</h1>
+			<p>This is a name slot</p>
+		</div>
+	</NamedSlot>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import NamedSlot from './components/NamedSlot.vue'
+
+export default {
+  name: 'App',
+  components: {
+	HelloWorld,
+	NamedSlot
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ slot_demo/src/components/NamedSlot.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<slot name="NamedSlot"></slot>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'NamedSlot'
+}
+</script>

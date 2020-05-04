b428b19c3dd87ca5cc14be6867f1c93295973ec5
Nalin Vaidya <nalinvaidya@gmail.com>
2020-05-04 10:23:19 +0700

    Added a application which demonstrate the Vue.js component life cycle.

---
+++ component_life_cycle/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>Vue.js Component life cycle</h1>
+    <HelloWorld v-if="isComponentAlive" msg="This is a basic application which demonstrate the Vue.js component life cycle"/>
+	<button v-on:click="destroyComponet">Destroy component</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data: function() {
+	  return {
+		  isComponentAlive: true
+	  }
+  },
+  methods: {
+	  destroyComponet() {
+		  this.isComponentAlive = false
+	  }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ component_life_cycle/src/components/HelloWorld.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+	<p>Counter : {{ counter }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data: function() {
+	  return {
+		  counter: 0
+	  }
+  },
+  beforeCreate: function() {
+	  console.log("In before create");
+  },
+  created: function() {
+	  console.log("In created");
+	  setInterval(() => {
+		  this.counter++;
+	  }, 3000)
+  },
+  beforeUpdate: function() {
+	  console.log("In before update with counter : ", this.counter);
+  },
+  updated: function() {
+	  console.log("In updated");
+  },
+  beforeMount: function() {
+	  console.log("In before mount");
+  },
+  mounted: function() {
+	  console.log("In mounted");
+  },
+  beforeDestroy: function() {
+	  console.log("In before destroy");
+  },
+  destroyed: function() {
+	  console.log("In destroyed");
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
